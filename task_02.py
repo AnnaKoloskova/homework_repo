be2a9915fc4e0712fd812d0a13d36f2962c9ebcf
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-01-16 18:21:53 +0300

    Выполнено

--- task_02.py
+++ task_02.py
@@ -4,3 +4,7 @@
 def duty_free(price, discount, holiday_cost):
     x=price*discount*0.01
     return int(holiday_cost/x)
+
+# Все задачи про алкоголь?!?))))))))))))
+# Отличная мотивация в новогдние праздники решать задачи)
+# PS Спасибо! Было интересно порешать задачи)
